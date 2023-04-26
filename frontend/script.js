e6789a685ea7e20f006e22c4ac0c46be3bb541b6
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2023-04-26 22:13:26 +0100

    adds project files

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+
+function init() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    }).addTo(map);
+}

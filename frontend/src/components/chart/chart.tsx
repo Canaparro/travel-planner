1c4011d4a6bed0f253a632e3bab0cfb129705163
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2023-05-07 16:50:58 +0100

    Fixes lint

--- frontend/src/components/chart/chart.tsx
+++ frontend/src/components/chart/chart.tsx
@@ -6,26 +6,26 @@ import { LinearScale, CategoryScale, PointElement, LineElement, Chart as Chartjs
 
 Chartjs.register(CategoryScale, LinearScale, PointElement, LineElement);
 
-export default function Chart({currentPosition}: {currentPosition: CurrentPosition}) {
+export default function Chart({currentPosition}: {currentPosition: CurrentPosition | null}) {
     const [temperature, setTemperature] = useState<ChartData<"line"> | null>(null);
 
     useEffect(() => {
         if(currentPosition !== null) {
             fetch(`https://api.open-meteo.com/v1/forecast?latitude=${currentPosition.lat}&longitude=${currentPosition.lng}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
-                .then((res) => res.json())
-                .then((data) => {
-                    let hours = data.hourly.time.map((date: string) => DateTime.fromISO(date).toFormat("HH'h' dd-MMM"));
-                    let chartData = {
-                        labels: hours,
-                        datasets: [{
-                            label: 'Temperature in C',
-                            data: data.hourly.temperature_2m,
-                            borderColor: "blue",
-                            fill: true,
-                            pointRadius: 2
-                        }]
-                    }
-                    setTemperature(chartData);
+            .then((res) => res.json())
+            .then((data) => {
+                let hours = data.hourly.time.map((date: string) => DateTime.fromISO(date).toFormat("HH'h' dd-MMM"));
+                let chartData = {
+                    labels: hours,
+                    datasets: [{
+                        label: 'Temperature in C',
+                        data: data.hourly.temperature_2m,
+                        borderColor: "blue",
+                        fill: true,
+                        pointRadius: 2
+                    }]
+                }
+                setTemperature(chartData);
             });
         }
     }, [currentPosition])
--- frontend/src/components/map/travelMap.tsx
+++ frontend/src/components/map/travelMap.tsx
@@ -6,7 +6,7 @@ import { ScriptProps } from 'next/script'
 export default function travelMap({setCurrentPosition}) {
     return (
         <MapContainer className={styles.map} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-            // TODO: replace with open street map
+            {/* TODO: replace with open street map */}
             <TileLayer url='http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
                 subdomains={['mt0', 'mt1', 'mt2', 'mt3']} />
             <LocationMarker setCurrentPosition={setCurrentPosition}/>

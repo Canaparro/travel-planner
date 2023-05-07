3e26aeac94c74400c6e817ed1c1803b5392cb602
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2023-05-07 16:46:06 +0100

    Fixes chart to work properly with userEffect

--- frontend/src/components/chart/chart.tsx
+++ frontend/src/components/chart/chart.tsx
@@ -1,46 +1,37 @@
-import styles from '@/styles/Home.module.css'
 import CurrentPosition from '../currentPosition';
 import { Line } from 'react-chartjs-2';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DateTime } from 'luxon';
-import { LinearScale, CategoryScale, PointElement, LineElement, Chart as Chartjs } from "chart.js";
+import { LinearScale, CategoryScale, PointElement, LineElement, Chart as Chartjs, ChartData } from "chart.js";
 
 Chartjs.register(CategoryScale, LinearScale, PointElement, LineElement);
 
 export default function Chart({currentPosition}: {currentPosition: CurrentPosition}) {
-    const [data, setData] = useState(null);
+    const [temperature, setTemperature] = useState<ChartData<"line"> | null>(null);
 
-    // if(currentPosition !== null) {
-    //     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${currentPosition.lat}&longitude=${currentPosition.lng}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             let hours = data.hourly.time.map((date: string) => DateTime.fromISO(date).toFormat("HH'h' dd-MMM"));
-    //             let myData = {
-    //                 labels: hours,
-    //                 datasets: [{
-    //                     label: 'Temperature in C',
-    //                     data: data.hourly.temperature_2m,
-    //                     borderColor: "blue",
-    //                     fill: true,
-    //                     pointRadius: 2
-    //                 }]
-    //             }
-    //             setData(myData);
-    //     });
-    // }
+    useEffect(() => {
+        if(currentPosition !== null) {
+            fetch(`https://api.open-meteo.com/v1/forecast?latitude=${currentPosition.lat}&longitude=${currentPosition.lng}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
+                .then((res) => res.json())
+                .then((data) => {
+                    let hours = data.hourly.time.map((date: string) => DateTime.fromISO(date).toFormat("HH'h' dd-MMM"));
+                    let chartData = {
+                        labels: hours,
+                        datasets: [{
+                            label: 'Temperature in C',
+                            data: data.hourly.temperature_2m,
+                            borderColor: "blue",
+                            fill: true,
+                            pointRadius: 2
+                        }]
+                    }
+                    setTemperature(chartData);
+            });
+        }
+    }, [currentPosition])
 
-    return data === null ? null :
+    return temperature === null ? null :
     (
-        <Line data={data}/>
+        <Line data={temperature}/>
     )
-    // return (
-        // <Line data={{
-        //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        //     datasets: [{
-        //       label: '# of Votes',
-        //       data: [12, 19, 3, 5, 2, 3],
-        //       borderWidth: 1
-        //     }]
-        //   }}/>
-    // )
 }
